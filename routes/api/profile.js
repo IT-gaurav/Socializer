1f8c5d203270184799a097a9fe94cf2b2cb9f4a4
IT-gaurav <56316270+IT-gaurav@users.noreply.github.com>
2020-10-07 12:23:56 -0400

    delete comments api

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -7,6 +7,10 @@ const { check, validationResult } = require("express-validator");
 const Profile = require("../../models/Profile");
 const User = require("../../models/User");
 
+const request = require("request");
+const config = require("config");
+const { response } = require("express");
+
 // @route   GET api/profile/me
 // @desc    Get current user profile
 // @access  Private
@@ -325,6 +329,38 @@ router.delete("/education/:edu_id", auth, async (req, res) => {
   } catch (err) {
     console.error(err.message);
     res.status(500).send("Server Error");
+  }
+});
+
+// ------------------------------------------
+// @route   GET api/profile/github/:username
+// @desc    Get user repos from github
+// @access  public
+
+router.get("/github/:username", (req, res) => {
+  try {
+    const options = {
+      uri: `https://api.github.com/users/${
+        req.params.username
+      }/repos?per_page=5&sort=created:asc&client_id=${config.get(
+        "githubClientId"
+      )}&client_secret=${config.get("githubSecret")}`,
+      method: "GET",
+      headers: { "user-agent": "node.js" },
+    };
+
+    request(options, (error, response, body) => {
+      if (error) console.error(error);
+
+      if (response.statusCode !== 200) {
+        res.status(404).json({ msg: "No github profile found" });
+      }
+
+      res.json(JSON.parse(body));
+    });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("server error");
   }
 });
 
